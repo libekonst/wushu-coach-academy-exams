88f257b3160ff8c60bf1702e30c881486c90b652
Konstantinos Liberopoulos <libekonst@gmail.com>
2023-04-06 19:13:05 +0300

    delete unused util

--- src/app/practice/[slug]/utils/arrayToRecord.ts
+++
@@ -1,9 +0,0 @@
-type Item<Value extends { id: string }> = { id: string; [key: string]: Value };
-
-type ArrayToRecord = <Value extends { id: string }>(
-  items: Item<Value>[]
-) => Record<string, Item<Value>>;
-
-const arrayToRecord: ArrayToRecord = (items) => {
-  return items.reduce((record, item) => ({ ...record, [item.id]: item }), {});
-};
