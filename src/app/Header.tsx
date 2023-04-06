2948a1e43bc985afdae0f0c47b2d52e74d8d64ff
Konstantinos Liberopoulos <libekonst@gmail.com>
2023-04-06 19:23:06 +0300

    remove sticky header

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -9,17 +9,13 @@ export const logoFont = Julius_Sans_One({
 
 export function Header() {
   return (
-    <header className="pl-2 pr-3 py-3 border-b flex justify-start items-center gap-4 sticky top-0 bg-white z-10">
+    <header className="pl-2 pr-3 py-3 border-b flex justify-start items-center gap-4 bg-white z-10">
       <Link
         href="/"
         className={`text-3xl font-bold text-[#b22234] ${logoFont.className}`}
       >
         Wushu Academy
       </Link>
-      {/* <ul className="flex justify-center items-center gap-2">
-        <Link href="/study">Study</Link>
-        <Link href="/practice">Practice</Link>
-      </ul> */}
       <div className="ml-auto">Psst! You are doing great 🐼</div>
     </header>
   );
